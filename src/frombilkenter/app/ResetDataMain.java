c0851942fcd6884486f330f57e433348c83b24e1
msaidk1 <msaidk1@gmail.com>
2026-04-08 20:08:26 +0300

    class done

--- src/frombilkenter/app/ResetDataMain.java
+++ src/frombilkenter/app/ResetDataMain.java
@@ -1,5 +1,14 @@
 package frombilkenter.app;
 
+import frombilkenter.data.AppState;
+import frombilkenter.data.MongoSyncManager;
+
 public class ResetDataMain {
-    
+    public static void main(String[] args) {
+        String mongoUri = System.getenv("FROM_BILKENTER_MONGO_URI");
+        String mongoDb = System.getenv("FROM_BILKENTER_MONGO_DB");
+        AppState appState = new AppState(new MongoSyncManager(mongoUri, mongoDb));
+        AppState.ActionResult result = appState.resetPersistenceToSeed();
+        System.out.println(result.message());
+    }
 }
