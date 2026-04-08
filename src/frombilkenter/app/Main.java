95823d17626f1e52ffd797a78bf9cfc3350971de
msaidk1 <msaidk1@gmail.com>
2026-04-08 19:12:09 +0300

    added main and show-er methods

--- src/frombilkenter/app/Main.java
+++ src/frombilkenter/app/Main.java
@@ -25,4 +25,38 @@ public class Main extends Application {
         showLogin();
         stage.show();
     }
+
+    public void showLogin() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginView.fxml"));
+        Parent root = loader.load();
+        LoginController controller = loader.getController();
+        controller.init(this, appState);
+        Scene scene = new Scene(root, 1440, 900);
+        scene.getStylesheets().add(getClass().getResource("/styles/app.css").toExternalForm());
+        primaryStage.setScene(scene);
+    }
+
+    public void showShell() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ShellView.fxml"));
+        Parent root = loader.load();
+        ShellController controller = loader.getController();
+        controller.init(this, appState);
+        Scene scene = new Scene(root, 1440, 900);
+        scene.getStylesheets().add(getClass().getResource("/styles/app.css").toExternalForm());
+        primaryStage.setScene(scene);
+    }
+
+    public void showSignUp() throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SignUpView.fxml"));
+        Parent root = loader.load();
+        SignUpController controller = loader.getController();
+        controller.init(this, appState);
+        Scene scene = new Scene(root, 1440, 900);
+        scene.getStylesheets().add(getClass().getResource("/styles/app.css").toExternalForm());
+        primaryStage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
